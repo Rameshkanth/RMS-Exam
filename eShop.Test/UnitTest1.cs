7b42bf4267178df5eb292812a859979c1f58b757
Rameshkanth <t_rameshkanth@yahoo.com>
2017-11-12 19:22:41 +0000

    Initial project setup

---
+++ eShop.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eShop.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
